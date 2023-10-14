9beab22c7baf1ad30e45efb19dee3713af4270f3
alphacoderds <rahmandapr20@gmail.com>
2023-10-14 15:44:51 +0700

    Membuat callback didalam class WordRoomDatabase

--- app/src/main/java/com/example/roomwordsample/WordRoomDatabase.kt
+++ app/src/main/java/com/example/roomwordsample/WordRoomDatabase.kt
@@ -4,13 +4,37 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 // Annotates class to be a Room Database with a table (entity) of the Word class
 @Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
 public abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
+
+    private class WorldDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback(){
+
+        overide fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    var wordDao = database.wordDao()
+
+                    suspend fun populateDatabase(wordDao: WordDao) {
+                        wordDao.deleteAll()
+                        var word = Word("Hello")
+                        wordDao.insert(word)
+                        word = Word("World!")
+                        wordDao.insert(word)
+                    }
+                }
+            }
+        }
+    }
 
     companion object {
         // Singleton prevents multiple instances of database opening at the
